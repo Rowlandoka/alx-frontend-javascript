b47d26b1da900d78dda7589287fc86e81346d1ef
Rowland Oka <rowlandoka@gmail.com>
2023-09-28 03:59:15 +0100

    create handlers and return all promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,6 +5,7 @@ export default function handleProfileSignup() {
   const user = createUser();
   return Promise.all([photo, user]).then(
     (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
+    // eslint-disable-next-line comma-dangle
     () => console.log('Signup system offline')
   );
 }
